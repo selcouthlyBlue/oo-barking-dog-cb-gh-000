9829422b3187d4577832283d0176429ed4dee808
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-08 03:41:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    return @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+cat = Dog.new
+cat.name = "Cat"
+cat.bark
